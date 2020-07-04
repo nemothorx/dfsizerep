3376df066cd760cca19c0f58eadd47d4e6c37699
Nemo Thorx <nemo@thorx.net>
2020-07-04 17:01:50 +1000

    rework output to calculate % as a float

--- dfsizerep.sh
+++ dfsizerep.sh
@@ -2,6 +2,11 @@
 
 # $mounts defaults to a calculated array of all possible fs
 
+# note: makes assumptions about the output of `/bin/df`
+# unusual df outputs not handled
+
+
+
 if [ -n "$1" ] ; then
     mounts=$@
     # bad params will fail the `grep` test in the loop below
@@ -13,11 +18,20 @@ date=$(date +\%F\ \%H:\%M:00)
 
 
 for mount in $mounts ; do
+    outfile=/${mount}/dfsizerep.history
+#    outfile=/dev/stdout         # simplest debug
     mount | grep -q " $mount " && ( \
         echo -n "$date," ; df -B1048576 $mount \
             | grep -v Filesystem \
-            | tr -s ' ' , \
-            | sed -e "s^,$mount^,$HOSTNAME:$mount^g"
-    ) >> /${mount}/dfsizerep.history
+            | tr -s ' ' \
+            | while read dev size used avail pct Mount ; do
+                # two possible percentages
+                # they differ because of the hidden reserved blocks. 
+                # In real world, I'm finding actual DF gives an answer between these two!
+                # df code for future review: https://git.savannah.gnu.org/cgit/coreutils.git/tree/src/df.c
+                pctcalc1=$(echo "scale=2;$used*100/$size" | bc)
+                pctcalc2=$(echo "scale=2;($size-$avail)*100/$size" | bc)
+                echo "$dev,$size,$used,$avail,$pctcalc2,$HOSTNAME:$mount"
+            done ) >> $outfile
 done
 
