c2ea60bde212748dd36bda0190d62a8a56aa50c5
Nemo Thorx <nemo@thorx.net>
2020-07-04 20:44:40 +1000

    df now using --output. Extra comments added

--- dfsizerep.sh
+++ dfsizerep.sh
@@ -2,10 +2,10 @@
 
 # $mounts defaults to a calculated array of all possible fs
 
-# note: makes assumptions about the output of `/bin/df`
-# unusual df outputs not handled
-
-
+# note: uses on the --output format of GNU df 
+# ...used to defensively give output same as default (as of coreutils 8.30)
+# This has not been tested on any alternative df (including historic GNU version)
+# ...any alternate df which outputs that same format and silently ignores --output should work
 
 if [ -n "$1" ] ; then
     mounts=$@
@@ -19,15 +19,16 @@ date=$(date +\%F\ \%H:\%M:00)
 
 for mount in $mounts ; do
     outfile=/${mount}/dfsizerep.history
+    # TODO: make this output to stdout if run as non-priv user
 #    outfile=/dev/stdout         # simplest debug
     mount | grep -q " $mount " && ( \
-        echo -n "$date," ; df -B1048576 $mount \
+        echo -n "$date," ; df -B1048576 --output=source,size,used,avail,pcent,target $mount \
             | grep -v Filesystem \
             | tr -s ' ' \
             | while read dev size used avail pct Mount ; do
                 # two possible percentages
                 # they differ because of the hidden reserved blocks. 
-                # In real world, I'm finding actual DF gives an answer between these two!
+                # In real world, I'm finding ACTUAL df gives an answer between these two!
                 # df code for future review: https://git.savannah.gnu.org/cgit/coreutils.git/tree/src/df.c
                 pctcalc1=$(echo "scale=2;$used*100/$size" | bc)
                 pctcalc2=$(echo "scale=2;($size-$avail)*100/$size" | bc)
