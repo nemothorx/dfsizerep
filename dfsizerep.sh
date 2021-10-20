18758c2c3579170a8aaa2cc5fe798cb688b45323
Nemo Thorx <nemo@thorx.net>
2021-10-20 21:16:28 +1000

    central cache of all data created when run as uid0

--- dfsizerep.sh
+++ dfsizerep.sh
@@ -2,7 +2,7 @@
 
 # $mounts defaults to a calculated array of all possible fs
 
-# note: uses on the --output format of GNU df 
+# note: uses the --output option to GNU df 
 # ...used to defensively give output same as default (as of coreutils 8.30)
 # This has not been tested on any alternative df (including historic GNU version)
 # ...any alternate df which outputs that same format and silently ignores --output should work
@@ -15,10 +15,11 @@ else
 fi
 
 date=$(date +\%F\ \%H:\%M:00)
-
+cachedir=/var/cache/dfsizerep
+# TODO: if I'm root then test/create $cachedir
 
 for mount in $mounts ; do
-    outfile=/${mount}/dfsizerep.history     # default design is write logs
+    outfile=${mount}/dfsizerep.history     # default design is write logs
     [ $UID -ne 0 ] && outfile=/dev/stdout   # non-root users get results to stdout
     mount | grep -q " $mount " && ( \
         echo -n "$date," ; df -B1048576 --output=source,size,used,avail,pcent,target $mount \
@@ -33,5 +34,19 @@ for mount in $mounts ; do
                 pctcalc2=$(echo "scale=2;($size-$avail)*100/$size" | bc)
                 echo "$dev,$size,$used,$avail,$pctcalc2,$HOSTNAME:$mount"
             done ) >> $outfile
+    if [ $UID -eq 0 ] ; then
+        # if we're running as a system job... 
+        # (this should perhaps also test for terminal/cronjob too?)
+        # ...then save a copy to a cache
+        cachefile=$(echo "$outfile" | tr / _)
+        # this ensures we have a full history of the dfsizerep per *mountpoint*
+        # ...this is distinct from the report on-disk which is per-disk. 
+        # ...I mainly use this for /backup/disk which is a mountpoint that rotates between multiple disks. 
+        # Primary cache kept within /root for cross-system longevity
+        # TODO: test/create /root/$cachedir
+        cat $outfile /root/$cachedir/$cachefile | sort -g | uniq > /root/$cachedir/$cachefile.tmp && mv /root/$cachedir/$cachefile.tmp /root/$cachedir/$cachefile
+        # ...and copied to system cache for access by other users
+        cp -a /root/$cachedir/$cachefile $cachedir/$cachefile
+    fi
 done
 
